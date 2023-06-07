80354bc9f640c75e0f11ee95a51ea3b5de93cd68
l1mey112 <l-m@l-m.dev>
2023-06-07 11:50:25 +1000

    change

--- t.v
+++ t.v
@@ -8,10 +8,10 @@ fn main() {
 	         https://vosca.dev/images/logo/test.webm
 	         https://l-m.dev/'
 
-	s := re.replace_by_fn(text, fn (r regex.RE, text string, b1 int, b2 int) string {
+	s := re.replace_by_fn(text, fn (_ regex.RE, text string, b1 int, b2 int) string {
 		t := text[b1..b2]
 
-		if t.ends_with('.mpv') || t.ends_with('webm') {
+		if t.ends_with('.mp4') || t.ends_with('webm') {
 			return '\n<video preload="none" src="${t}" controls></video>\n'
 		}
 		return '\n<img loading="lazy" src="${t}">\n'
