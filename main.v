b13af5e60e10eec2ddcb9d65054b301bd9ca8917
l-m <l-m@l-m.dev>
2023-06-07 17:51:19 +1000

    refac

--- main.v
+++ main.v
@@ -3,7 +3,6 @@ import time
 import regex
 import mymarkdown
 
-// sets a custom table name. Default is struct name (case-sensitive)
 [table: 'posts']
 struct Post {
 	id int [primary; sql: serial]
@@ -14,8 +13,14 @@ mut:
 	content string
 }
 
-fn preprocess(mut media_regex regex.RE, text string) string {
-	ntext := media_regex.replace_by_fn(text, fn (_ regex.RE, text string, b1 int, b2 int) string {
+struct App {
+mut:
+	media_regex regex.RE = regex.regex_opt(r'https?://\S+\.(?:(png)|(jpe?g)|(gif)|(svg)|(webp)|(mp4)|(webm))') or { panic(err) }
+	db sqlite.DB = sqlite.connect("data.sqlite") or { panic(err) }
+}
+
+fn (mut a App) preprocess(text string) string {
+	ntext := a.media_regex.replace_by_fn(text, fn (_ regex.RE, text string, b1 int, b2 int) string {
 		t := text[b1..b2]
 
 		if t.ends_with('.mp4') || t.ends_with('webm') {
@@ -28,17 +33,15 @@ fn preprocess(mut media_regex regex.RE, text string) string {
 }
 
 fn main() {
-	mut db := sqlite.connect("data.sqlite")!
-	defer { db.close() or { panic(err) } }
+	mut app := App{}
+	defer { app.db.close() or { panic(err) } }
 
-	mut media_regex := regex.regex_opt(r'https?://\S+\.(?:(png)|(jpe?g)|(gif)|(svg)|(webp)|(mp4)|(webm))')!
-
-	posts := sql db {
+	posts := sql app.db {
 		select from Post
 	}!
 
 	for p in posts {
-		v := preprocess(mut media_regex, p.content)
+		v := app.preprocess(p.content)
 		println(v)
 		println('---------------------')
 	}
