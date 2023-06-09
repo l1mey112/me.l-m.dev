0978d043b722f5ab4bce58de95fd31a20d78d063
l1mey112 <l-m@l-m.dev>
2023-06-10 00:10:19 +1000

    caching and search

--- src/main.v
+++ src/main.v
@@ -8,12 +8,72 @@ import compress.gzip
 import net.urllib
 import time
 
+const cache_max = 8
+
 [heap]
 struct App {
 mut:
 	media_regex regex.RE
 	db sqlite.DB
-	prerendered_home string
+	cache []CacheEntry = []CacheEntry{cap: cache_max}
+}
+
+fn (mut app App) invalidate_cache() {
+	unsafe { app.cache.len = 0 }
+}
+
+fn (mut app App) get_cache(query Query) ?string {
+	// get cache value, increment pop
+
+	for mut c in app.cache {
+		if c.query == query {
+			c.pop++
+			return c.render
+		}
+	}
+
+	return none
+}
+
+fn (mut app App) enter_cache(query Query, render string) {
+	// get_cache() should be called before this
+	// no point to enter something that is already in the cache
+
+	if app.cache.len < cache_max {
+		app.cache << CacheEntry{
+			query: query
+			render: render
+		}
+		return
+	}
+
+	mut cc_idx := 0
+
+	// locate the cache entry with the lowest pop
+	for idx, c in app.cache {
+		if c.pop < app.cache[cc_idx].pop {
+			cc_idx = idx
+		}
+	}
+
+	// wipe out old cache value, set pop to 0
+	app.cache[cc_idx] = CacheEntry{
+		query: query
+		render: render
+	}
+}
+
+struct Query {
+mut:
+	search string
+	tags []string
+}
+
+struct CacheEntry {
+	query Query
+	render string
+mut:
+	pop u64
 }
 
 fn (app &App) fmt_tag(tags []string) string {
@@ -36,10 +96,10 @@ fn (app &App) fmt_tag(tags []string) string {
 	return sb.str()
 }
 
-struct Query {
-mut:
-	search string
-	tags []string
+fn (app &App) dbg_log() {
+	for idx, c in app.cache {
+		eprintln("${idx}: (${c.pop}) '${c.query.search}' > ${c.query.tags}")
+	}
 }
 
 fn get_query(req string) Query {
@@ -82,11 +142,11 @@ fn (app &App) raw_query(query string) ![]sqlite.Row {
 }
 
 const sqlsearch_replace = [
-	"\t" "*"
-	"\n" "*"
-	"\v" "*"
-	"\f" "*"
-	"\r" "*"
+	"\t", "*"
+	"\n", "*"
+	"\v", "*"
+	"\f", "*"
+	"\r", "*"
 	" ", "*"
 	"'", "''" // sqlescape
 ]
@@ -106,6 +166,15 @@ fn (mut app App) serve_home(req string, mut res phttp.Response) {
 	// \-- return
 
 	query := get_query(req)
+
+	if render := app.get_cache(query) {
+		res.http_ok()
+		res.header_date()
+		res.html()
+		write_all(mut res, render) // TODO: handle gzip
+		return
+	}
+	eprintln("cache MISS: ${query}")
 
 	mut db_query := "select * from posts"
 
@@ -135,33 +204,21 @@ fn (mut app App) serve_home(req string, mut res phttp.Response) {
 	mut all_tags := tag_rows.map(Tag{it.vals[0], it.vals[1].int()})
 	all_tags.sort(a.count > b.count)
 
-	eprintln(posts)
-	eprintln(all_tags)
-	eprintln(db_query)
-}
-
-fn (mut app App) rerender()! {
 	posts_total := sql app.db {
 		select count from Post
-	}!
+	} or { panic(err) }
 
-	tag_rows, tag_ret := app.db.exec(query_all_tags)
-	if sqlite.is_error(tag_ret) {
-		return error('error: ${tag_ret}')
-	}
+	render := $tmpl('tmpl.html')
+	app.enter_cache(query, render)
 
-	mut all_tags := tag_rows.map(Tag{it.vals[0], it.vals[1].int()})
-	all_tags.sort(a.count > b.count)
+	res.http_ok()
+	res.header_date()
+	res.html()
+	write_all(mut res, render) // TODO: handle gzip
 
-	//all_tags_fmt := all_tags.map("${it.tag}: ${it.count}")
-
-	posts := sql app.db {
-		select from Post
-	}!
-
-	// TODO: search args, then cache that
-
-	app.prerendered_home = $tmpl('tmpl.html')
+	// eprintln(posts)
+	// eprintln(all_tags)
+	// eprintln(db_query)
 }
 
 fn callback(data voidptr, req phttp.Request, mut res phttp.Response) {
@@ -183,12 +240,13 @@ fn callback(data voidptr, req phttp.Request, mut res phttp.Response) {
 	_ = is_authed
 
 	if phttp.cmpn(req.method, 'GET ', 4) {
-		if phttp.cmp(req.path, '/') {
-			app.serve_home(req.path, mut res)
+		if phttp.cmp(req.path, '/') || phttp.cmpn(req.path, '/?', 2) {
 			res.http_ok()
 			res.header_date()
 			res.html()
-			if accepts_gzip {
+			app.serve_home(req.path, mut res)
+			app.dbg_log()
+			/* if accepts_gzip {
 				// completely cuts the HTML size in HALF!
 				// TODO: this should be cached, it also creates unneeded copies
 
@@ -197,11 +255,7 @@ fn callback(data voidptr, req phttp.Request, mut res phttp.Response) {
 				write_all(mut res, val.bytestr())
 			} else {
 				write_all(mut res, app.prerendered_home)
-			}
-		} else if phttp.cmpn(req.path, '/?', 2) {
-			app.serve_home(req.path, mut res)
-			res.http_404()
-			res.end()
+			} */
 		} else if phttp.cmp(req.path, '/TerminusTTF.woff2') {
 			res.http_ok()
 			res.header_date()
@@ -234,7 +288,6 @@ fn main() {
 		exit(0)
 	})!
 
-	app.rerender()!
 	println("http://localhost:8080/")
 	picoev.new(port: 8080, cb: &callback, user_data: app, max_write: 8192).serve() // RIGHT UNDER THE MAXIMUM i32 SIGNED VALUE
 }
