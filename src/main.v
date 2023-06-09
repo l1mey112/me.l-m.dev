b96c99f4b259b50b32fa9ec6d20a004301ccb12b
l-m <l-m@l-m.dev>
2023-06-09 16:43:10 +1000

    src change

--- src/main.v
+++ src/main.v
@@ -1,22 +1,12 @@
 import db.sqlite
 import time
 import regex
-import mymarkdown
 import mypicoev as picoev
 import mypicohttpparser as phttp
 import os
 import strings
 import compress.gzip
 import net.http
-
-[table: 'posts']
-struct Post {
-	id int [primary; sql: serial]
-	created_at time.Time
-	tags string // space separated
-mut:
-	content string
-}
 
 [heap]
 struct App {
@@ -46,43 +36,6 @@ fn (app &App) fmt_tag(tags []string) string {
 	return sb.str()
 }
 
-fn (mut app App) preprocess(text string) string {
-	ntext := app.media_regex.replace_by_fn(text, fn (_ regex.RE, text string, b1 int, b2 int) string {
-		t := text[b1..b2]
-
-		if t.ends_with('mp4') || t.ends_with('webm') {
-			return '\n<video preload="none" src="${t}" controls></video>\n'
-		}
-		return '\n<img loading="lazy" src="${t}">\n'
-	})
-
-	return mymarkdown.to_html(ntext)
-}
-
-const query_all_tags = "WITH split(tag, tags_remaining) AS (
-  -- Initial query
-  SELECT 
-    '',
-    tags || ' ' -- Appending tags column data to handle the first tag
-  FROM posts
-  -- Recursive query
-  UNION ALL
-  SELECT
-    trim(substr(tags_remaining, 0, instr(tags_remaining, ' '))),
-    substr(tags_remaining, instr(tags_remaining, ' ') + 1)
-  FROM split
-  WHERE tags_remaining != ''
-)
-SELECT tag, COUNT(*) AS tag_count
-FROM split
-WHERE tag != ''
-GROUP BY tag;"
-
-struct Tag {
-	tag string
-	count int
-}
-
 fn (mut app App) rerender()! {
 	posts_total := sql app.db {
 		select count from Post
@@ -105,32 +58,6 @@ fn (mut app App) rerender()! {
 	// TODO: search args, then cache that
 
 	app.prerendered_home = $tmpl('tmpl.html')
-}
-
-fn write_all(mut res phttp.Response, v string) {
-	res.write_string('Content-Length: ')
-	unsafe {
-		res.buf += C.u64toa(&char(res.buf), v.len)
-	}
-	res.write_string('\r\n\r\n')
-
-	if (i64(res.buf) - i64(res.buf_start) + i64(v.len)) >= 8192 {
-		res.end()
-		C.write(res.fd, v.str, v.len)
-	} else {
-		res.write_string(v)
-		res.end()
-	}
-}
-
-const terminus = $embed_file('TerminusTTF.woff2').to_string()
-
-fn mcmp(a &char, len int, b string) bool {
-	if len != b.len {
-		return false
-	}
-
-	return unsafe { C.memcmp(a, b.str, b.len) == 0 }
 }
 
 fn callback(data voidptr, req phttp.Request, mut res phttp.Response) {
---
+++ src/utils.v
@@ -0,0 +1,76 @@
+import mypicohttpparser as phttp
+import mymarkdown
+import time
+import regex
+
+[table: 'posts']
+struct Post {
+	id int [primary; sql: serial]
+	created_at time.Time
+	tags string // space separated
+mut:
+	content string
+}
+
+fn mcmp(a &char, len int, b string) bool {
+	if len != b.len {
+		return false
+	}
+
+	return unsafe { C.memcmp(a, b.str, b.len) == 0 }
+}
+
+const terminus = $embed_file('TerminusTTF.woff2').to_string()
+
+const query_all_tags = "WITH split(tag, tags_remaining) AS (
+  -- Initial query
+  SELECT 
+    '',
+    tags || ' ' -- Appending tags column data to handle the first tag
+  FROM posts
+  -- Recursive query
+  UNION ALL
+  SELECT
+    trim(substr(tags_remaining, 0, instr(tags_remaining, ' '))),
+    substr(tags_remaining, instr(tags_remaining, ' ') + 1)
+  FROM split
+  WHERE tags_remaining != ''
+)
+SELECT tag, COUNT(*) AS tag_count
+FROM split
+WHERE tag != ''
+GROUP BY tag;"
+
+struct Tag {
+	tag string
+	count int
+}
+
+fn write_all(mut res phttp.Response, v string) {
+	res.write_string('Content-Length: ')
+	unsafe {
+		res.buf += C.u64toa(&char(res.buf), v.len)
+	}
+	res.write_string('\r\n\r\n')
+
+	if (i64(res.buf) - i64(res.buf_start) + i64(v.len)) >= 8192 {
+		res.end()
+		C.write(res.fd, v.str, v.len)
+	} else {
+		res.write_string(v)
+		res.end()
+	}
+}
+
+fn (mut app App) preprocess(text string) string {
+	ntext := app.media_regex.replace_by_fn(text, fn (_ regex.RE, text string, b1 int, b2 int) string {
+		t := text[b1..b2]
+
+		if t.ends_with('mp4') || t.ends_with('webm') {
+			return '\n<video preload="none" src="${t}" controls></video>\n'
+		}
+		return '\n<img loading="lazy" src="${t}">\n'
+	})
+
+	return mymarkdown.to_html(ntext)
+}
