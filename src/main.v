ebb6e719a26af9638a8fe330b4292a8c1b94bf42
l-m <l-m@l-m.dev>
2023-06-17 16:52:02 +1000

    rss

--- src/main.v
+++ src/main.v
@@ -316,7 +316,7 @@ fn (mut app App) serve_home(req string, is_authed bool, use_gzip bool, mut res p
 		}
 
 		post := rows[0]
-		description := "${post.created_at.utc_string()} + ${app.fmt_tag(post.tags.split(' '))}"
+		description := "#${post.created_at} ${app.fmt_tag(post.tags.split(' '))}"
 
 		res.http_ok()
 		res.header_date()
